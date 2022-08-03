9795f1c672973278273fadfcfaf8b4ed554f5232
Julia <jskredova@yandex.ru>
2022-08-03 21:54:11 +0300

    add Example 003 Sum

---
+++ Examples/Example003_Sum/Program.cs
@@ -0,0 +1,4 @@
+﻿int numberA = 31;   //  определили первую переменную 
+int numberB = 5;   //  определили вторую переменную 
+int result = numberA + numberB;  //  промежуточная переменная
+Console.WriteLine(result);
