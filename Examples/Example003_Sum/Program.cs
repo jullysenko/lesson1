4afd6df9a8cbbac82e0228459bc7dd6ecd5e42fe
Julia <jskredova@yandex.ru>
2022-08-03 23:19:43 +0300

    add random

--- Examples/Example003_Sum/Program.cs
+++ Examples/Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;   //  определили первую переменную 
-int numberB = 5;   //  определили вторую переменную 
+﻿int numberA = new Random().Next(1,10);   //  определили первую переменную 
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);   //  определили вторую переменную 
+Console.WriteLine(numberB);
 int result = numberA + numberB;  //  промежуточная переменная
 Console.WriteLine(result);
