b16ab9b01a0b1296d5ad535e49f88f721e581013
Julia <jskredova@yandex.ru>
2022-08-24 23:43:01 +0300

    add Example 013

---
+++ Examples/Example013_RecursionAlgorrithm/Program.cs
@@ -0,0 +1,43 @@
+﻿string[,] table = new string[2, 5];
+table[1,2] = "слово";   // обратиться к нужному элементу - обычная переменная
+
+// for(int rows = 0; rows < 2; rows++)
+// {
+//     for(int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+
+
+
+void PrintArray(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)  // GetLength(0) - получить кол-во строк
+    {
+        for(int j = 0; j < matr.GetLength(1); j++) // GetLength(1) - получить кол-во столбцов
+        { 
+            Console.Write($"{matr[i,j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+
+void FillArray(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        { 
+            matr[i,j] = new Random().Next(1,10); 
+        }    
+    }
+}
+int[,] matrix = new int[3,4];
+
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
