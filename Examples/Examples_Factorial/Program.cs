a643793077a0dde57e6418e2dd4dcafec84a16b3
Julia <jskredova@yandex.ru>
2022-08-24 23:44:31 +0300

    add Factorial

---
+++ Examples/Examples_Factorial/Program.cs
@@ -0,0 +1,12 @@
+﻿double Factotial (int n)
+{
+    if (n == 1) return 1; // мы дошли до 1, то мы должны вернуть 1
+    else return n * Factotial(n - 1);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factotial(i)}");
+}
+
+
